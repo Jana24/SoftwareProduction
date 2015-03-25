cf3b0bf9e4a41b11401820bdbf25389a89af59d7
DialHouse25 <DialHouse25@DialHouse>
2015-03-25 20:29:30 +0000

    experimenting db connection as one standalone file to be referenced by other pages. - success

--- SoftwareProduction/topsearch.php
+++ SoftwareProduction/topsearch.php
@@ -6,23 +6,8 @@
  */
 
 // include login information
-  include('private/db_login.php');
-  // echo "host is $db_host  user is $db_username  password is  selected  database is $db_name";
-  // connect to database
-  $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-  //echo $connection;
-  if (!$connection){
-    die ("Could not connect to database: <br />". mysql_error());
-  }
+  include('db_connect.php');
   
-  // select the database
-  $db_select=  mysqli_select_db($connection, $db_name);
-  
-  //echo " db selected".$db_select;
-  
-  if (!$db_select){
-    die ("could not select the database: <br />". mysql_error());
-  }  
 echo '  <html>'; 
 echo '    <head>'; 
 echo '        <title>Search</title>'; 
