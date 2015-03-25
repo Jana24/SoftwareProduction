6709e49754c2568df56201f76053d6cdf1bb4e07
DialHouse25 <DialHouse25@DialHouse>
2015-03-25 20:34:53 +0000

    change database connection code to one line of include 'topsearch.php';

--- SoftwareProduction/editActivity.php
+++ SoftwareProduction/editActivity.php
@@ -5,21 +5,5 @@
     Author     : Juan Wang 13008700
  */
 
-// include login information
-  include('private/db_login.php');
-  // echo "host is $db_host  user is $db_username  password is  selected  database is $db_name";
-  // connect to database
-  $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-  //echo $connection;
-  if (!$connection){
-    die ("Could not connect to database: <br />". mysql_error());
-  }
-  
-  // select the database
-  $db_select=  mysqli_select_db($connection, $db_name);
-  
-  //echo " db selected".$db_select;
-  
-  if (!$db_select){
-    die ("could not select the database: <br />". mysql_error());
-  }
+// include top header and database connection
+include 'topsearch.php';
