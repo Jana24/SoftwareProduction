9d2b005c21b8550e9f2d1ccd7da3e0bca1eafa11
jwillmann <jwillmann@internal-student-7-254.eduroam.brookes.ac.uk>
2015-04-12 17:48:14 +0100

    linked first createProjects to second, did the login (without check), we can now get the username from the login to search all projects from that user

--- SoftwareProduction/db_connect.php
+++ SoftwareProduction/db_connect.php
@@ -7,12 +7,14 @@
 
 // include login information
   include('private/db_login.php');
+ 
   // echo "host is $db_host  user is $db_username  password is  selected  database is $db_name";
   // connect to database
   $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+
   //echo $connection;
   if (!$connection){
-    die ("Could not connect to database: <br />". mysql_error());
+    die ("Could not connect to database: <br />" . mysql_error());
   }
   
   // select the database
--- SoftwareProduction/insert_project.php
+++ SoftwareProduction/insert_project.php
@@ -7,7 +7,7 @@
 //session_start();
 
 // include login information
-include('private/db_login.php');  
+include('db_connect.php');  
 // echo "host is $db_host  user is $db_username  password is  selected  database is $db_name";
 // connect to database
 $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
@@ -31,7 +31,7 @@ $description = $_POST['proj_description'];
 $status = $_POST['status'];
 $start_date = $_POST['startDate'];
 $end_date = $_POST['endDate']; 
-print_r($_POST);
+
 //$query = "SELECT * FROM projects";
 
 $dateFormated = explode('/', $start_date);
@@ -40,23 +40,35 @@ $sdate = $dateFormated[2].'-'.$dateFormated[1].'-'.$dateFormated[0];
 $dateFormated = explode('/', $end_date);
 $edate = $dateFormated[2].'-'.$dateFormated[1].'-'.$dateFormated[0];
 
-
-print_r($end_date);
-print_r($edate);
+//check status, so the right one goes into DB:
+print_r("Status: " .$status);
+if($status == "not_started"){
+    print_r("1Status: " .$status);
+    $database_status = "not started";
+}else if($status == "ungoing"){
+    print_r("2Status: " .$status);
+    $database_status = "undergoing";
+}else if($status == "completed"){
+    print_r("3Status: " .$status);
+    $database_status = "finished";
+}
 
 $query = "INSERT INTO projects(";
 $query .= "name, status, description, managerId, budget, creationDate, startDate, dueDate";
 $query .= ") VALUES (";
-$query .= " '$projName', 'not started', '$description', 1, 1300.00, CURRENT_TIMESTAMP, '$sdate', '$edate'";
+$query .= " '$projName', '$database_status', '$description', 1, 1300.00, CURRENT_TIMESTAMP, '$sdate', '$edate'";
 $query .= ")";
 
 $result = mysqli_query($connection, $query);
 if($result){
     echo "SUCCESS!";
+    header("Location: createProject2.html" ); 
+    exit;    
 }
 else{
     die("Database query failed. " .mysql_error($connection));
-
+    echo "Something went wrong!";
+    header("Location: createProject1.html"); 
 }
 
 ?> 
