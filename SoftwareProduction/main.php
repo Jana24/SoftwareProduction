5538bd72bccd0c5b33cc6e57849436fe2dfadc29
danielmckean <danielmckean@danielmckean-PC>
2015-04-21 01:15:00 +0100

    Redesigned table on main page

--- SoftwareProduction/main.php
+++ SoftwareProduction/main.php
@@ -1,6 +1,7 @@
 <style type="text/css">
     .selectedTag{
-        background: #a0a0a0;
+        background: #eee;
+        font-weight:bold;
     }
  </style>
 
@@ -45,24 +46,28 @@ echo '      width:90%; ';
     //This is important in terms of keeping the table at a good size
 echo '      }';
 echo '      table,tr,th,td{ ';
-echo '      border: 1px solid black; ';
+echo '      border: 1px solid #D6D6D6; ';
 echo '      padding: 5px; ';
 echo '      border-collapse:collapse; ';
-echo '      margin-left:20px; ';
+echo '      margin-left:50px; ';
 echo '      font-size:14px; ';
 echo '      } ';
    
 echo '      h3 { ';
-echo '      padding-left:20px;  '; 
+echo '      padding-left:50px;  '; 
 echo '      } ';
-   
+
+//echo '           tr:nth-of-type(odd) { 
+ //           background: #eee; 
+ //           }
+ //  ';
 echo '     th { ';
-echo '      background-color:#D0D0D0 ;  ';
+echo '      background-color:#99CCFF ;  ';
 echo '      padding:6px;';
 echo '      } ';
 echo '      tr:hover { 
                 cursor: pointer;
-                background: grey; 
+                background: #eee; 
              }
              td a { 
                 display: block; 
@@ -86,8 +91,8 @@ echo '	    <table id="projectsTableMain" > ';
 echo '		<!-- create table 1 --> ';     
 echo '	      <tbody> ';
 echo '	        <tr> ';                  
-echo '          <td width="100px" align="left" style="background-color:#D0D0D0; padding:6px";>Name  </th> ';
-echo '          <td width="780px" align="left" style="background-color:#D0D0D0; padding:6px;";>Status  </th>';
+echo '          <td width="200px" align="left" style="background-color:#99CCFF; padding:6px; font-weight:bold";>Name  </th> ';
+echo '          <td width="750px" align="left" style="background-color:#99CCFF; padding:6px; font-weight:bold";>Status  </th>';
 echo '		</tr> ';
             $query2 = "select p.name, p.status, p.id from projects p, user_project up, users u where p.id=up.projectid and up.userId=u.id and u.name='$user';";
             $result2 = mysqli_query($connection, $query2);
@@ -124,28 +129,28 @@ echo '	      <thead> ';
 echo '	        <tr> ';
 echo '                  <th width="100px" align="left">Name  </th> ';
 echo '                  <th width = "200px" align="left">Project</th> ';
-echo '                  <th width= "295px" align="left">Due to</th> ';
+echo '                  <th width= "295px" align="left">Due</th> ';
 echo '                  <th width="100px" align="left">Days Left</th> ';
 echo '		  <th width ="135px" align="left">Status</th> ';
 echo '                </tr> ';
 echo '	      </thead> ';
 echo '		  <tbody> ';
 echo '		    <tr> ';
-echo '                <td>Juan Wang</td> ';
+echo '                <td style="padding:8px;">Juan Wang</td> ';
 echo '                <td>Project 2  </td> ';
 echo '                <td>15.02.2015</td> ';
 echo '                <td>2  </td> ';
 echo '                <td>pending</td> ';
 echo '              </tr> ';
 echo '		    <tr> ';
-echo '                <td>Daniel McKean</td> ';
+echo '                <td style="padding:8px;">Daniel McKean</td> ';
 echo '                <td>Project 5</td> ';
 echo '                <td>18.02.2015</td> ';
 echo '                <td>5  </td> ';
 echo '                <td>Not started</td> ';
 echo '              </tr> ';
 echo '              <tr> ';
-echo '		      <td>Jana Willmann</td> ';
+echo '		      <td style="padding:8px;">Jana Willmann</td> ';
 echo '                <td>Project 2</td>';
 echo '                <td>21.02.2015</td> ';
 echo '                <td>9  </td> ';
